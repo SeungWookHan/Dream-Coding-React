2f4257163ab6aa312cf784231aeee2536352f9e0
SeungWookHan <hswook12@me.com>
2021-07-18 00:59:22 +0900

    Add handleIncrement and handleDecrement

--- react-basic/habit-tracker/src/components/habit.jsx
+++ react-basic/habit-tracker/src/components/habit.jsx
@@ -1,15 +1,36 @@
 import React, { Component } from "react";
 
 export default class Habit extends Component {
+  state = {
+    count: 0,
+  };
+
+  handleIncrement = () => {
+    // state 오브젝트 count 증가 후 state 업데이트
+    // this.state.count += 1 식으로 업데이트하면 리액트에서 알 수 없음
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  handleDecrement = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
+
   render() {
     return (
       <li className="habit">
         <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-increase">
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
