476dd7b93476739e888301dbc05d5c1f5cf06245
SeungWookHan <hswook12@me.com>
2021-07-18 00:52:57 +0900

    Initialize habit-tracker project and Add css

---
+++ react-basic/habit-tracker/src/app.jsx
@@ -0,0 +1,8 @@
+import "./app.css";
+import Habit from "./components/habit";
+
+function App() {
+  return <Habit />;
+}
+
+export default App;
---
+++ react-basic/habit-tracker/src/components/habit.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class Habit extends Component {
+  render() {
+    return (
+      <li className="habit">
+        <span className="habit-name">Reading</span>
+        <span className="habit-count">8</span>
+        <button className="habit-button habit-increase">
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button className="habit-button habit-decrease">
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button className="habit-button habit-delete">
+          <i className="fas fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
+}
