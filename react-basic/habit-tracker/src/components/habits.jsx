0a65ffc6ab3630275e41d10c2f502f1bf320efcc
SeungWookHan <hswook12@me.com>
2021-07-20 00:15:29 +0900

    Refactoring habits component to functional component

--- react-basic/habit-tracker/src/components/habits.jsx
+++ react-basic/habit-tracker/src/components/habits.jsx
@@ -1,44 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import Habit from "./habit";
 import HabitAddForm from "./habitAddForm";
 
-export default class Habits extends Component {
-  handleIncrement = (habit) => {
-    this.props.onIncrement(habit);
-  };
+const Habits = ({
+  habits,
+  onIncrement,
+  onDecrement,
+  onDelete,
+  onAdd,
+  onReset,
+}) => {
+  return (
+    <div className="habits">
+      <HabitAddForm onAdd={onAdd} />
+      <ul>
+        {habits.map((habit) => (
+          <Habit
+            key={habit.id}
+            habit={habit}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            onDelete={onDelete}
+          />
+        ))}
+      </ul>
+      <button className="habits-reset" onClick={onReset}>
+        Reset All
+      </button>
+    </div>
+  );
+};
 
-  handleDecrement = (habit) => {
-    this.props.onDecrement(habit);
-  };
-
-  handleDelete = (habit) => {
-    this.props.onDelete(habit);
-  };
-
-  handleAdd = (name) => {
-    this.props.onAdd(name);
-  };
-
-  render() {
-    return (
-      <>
-        <HabitAddForm onAdd={this.handleAdd} />
-        <ul>
-          {this.props.habits.map((habit) => (
-            <Habit
-              key={habit.id}
-              habit={habit}
-              onIncrement={this.handleIncrement}
-              onDecrement={this.handleDecrement}
-              onDelete={this.handleDelete}
-              // 이 부분은 onDelete={(habit) => {this.props.handleDelete(habit)}} 으로도 표현 가능
-            />
-          ))}
-        </ul>
-        <button className="habits-reset" onClick={this.props.onReset}>
-          Reset All
-        </button>
-      </>
-    );
-  }
-}
+export default Habits;
