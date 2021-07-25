5ec9bbc022a8e5df03f339fdb8a6df7f03e86716
SeungWookHan <hswook12@me.com>
2021-07-26 00:29:08 +0900

    Add popular list when component mount

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,7 +1,28 @@
+import React, { useState, useEffect } from "react";
 import "./app.css";
+import VideoList from "./components/video_list/video_list";
+import * as config from "./config";
 
 function App() {
-  return <h1>Hello :)</h1>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect");
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/videos?key=${config.API_KEY}&part=snippet&chart=mostPopular&maxResults=25`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []); // 마운트가 되었을때만 호출
+
+  return <VideoList videos={videos} />;
 }
 
 export default App;
